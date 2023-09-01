e3068b9d6e558eaf565dae4135f8866712bc88a8
RiverDot <72624705+RiverDot@users.noreply.github.com>
2023-09-01 05:48:38 -0400

    added base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'http://riverdot.tech/godot-programing-game-docs/',
 	integrations: [
 		starlight({
 			title: 'Godot Programming Game',
